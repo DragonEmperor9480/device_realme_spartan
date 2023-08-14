167e0bdf718d2403f776e4227d476322241095a2
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2023-08-14 18:00:44 +0530

    spartan: build lineage health hal

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,9 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=realme
 TARGET_KERNEL_SOURCE := kernel/realme/sm8250
 TARGET_KERNEL_CONFIG := vendor/sm8250_defconfig
 
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
